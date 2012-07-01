87e3837cb37fcd7cc0d4291453ba5a4e0cc8b084
Marcel Beck <fo3nik5@gmail.com>
2012-07-01 14:57:17 +0200

    Initial Commit

---
+++ classes/pagination.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Pagination helper.
+ *
+ * @author Marcel Beck <marcel.beck@mbeck.org>
+ */
+class Pagination extends Kohana_Pagination {
+
+
+} // End Pagination
---
+++ config/pagination.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'source'            => 'route', // source: query or route
+	'key'               => 'page',
+	'first_page_in_url' => false,
+);
