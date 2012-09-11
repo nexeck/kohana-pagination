30c67c071c7ecf4aa92dd808af906778dc2e1d8b
Marcel Beck <fo3nik5@gmail.com>
2012-09-11 23:13:46 +0200

    Codestyle

--- classes/kohana/pagination.php
+++ classes/kohana/pagination.php
@@ -11,268 +11,255 @@
  * @author    Marcel Beck <marcel.beck@mbeck.org>
  * @copyright (c) 2012 Marcel Beck
  */
-abstract class Kohana_Pagination {
+abstract class Kohana_Pagination
+{
+    /**
+     * @var int
+     */
+    protected $_total;
 
-	/**
-	 * @var int
-	 */
-	protected $_total;
+    /**
+     * @var int
+     */
+    protected $_limit;
 
-	/**
-	 * @var int
-	 */
-	protected $_limit;
+    /**
+     * @var int
+     */
+    protected $_current;
 
-	/**
-	 * @var int
-	 */
-	protected $_current;
+    /**
+     * @var Request
+     */
+    protected $_request;
 
-	/**
-	 * @var Request
-	 */
-	protected $_request;
+    /**
+     * @var Route
+     */
+    protected $_route;
 
-	/**
-	 * @var Route
-	 */
-	protected $_route;
+    /**
+     * @var array Parameters to use with Route to create URIs
+     */
+    protected $_route_params = array();
 
-	/**
-	 * @var array Parameters to use with Route to create URIs
-	 */
-	protected $_route_params = array();
+    /**
+     * @var Kohana_Config_Group
+     */
+    protected $_config;
 
-	/**
-	 * @var Kohana_Config_Group
-	 */
-	protected $_config;
+    /**
+     * @param int $total
+     * @param int $limit
+     * @param int $current
+     *
+     * @uses Request::current
+     * @uses Kohana::$config
+     */
+    public function __construct($total, $limit = null, $current = null)
+    {
+        $this->request(Request::current());
+        $this->route($this->_request->route());
 
+        $this->_route_params = array(
+                                   'directory'    => $this->_request->directory(),
+                                   'controller'   => $this->_request->controller(),
+                                   'action'       => $this->_request->action(),
+                               ) + $this->_request->param();
 
-	/**
-	 * @param int $total
-	 * @param int $limit
-	 * @param int $current
-	 *
-	 * @uses Request::current
-	 * @uses Kohana::$config
-	 */
-	public function __construct($total, $limit = null, $current = null)
-	{
-		$this->request(Request::current());
-		$this->route($this->_request->route());
+        $this->_config = Kohana::$config->load('pagination');
 
-		$this->_route_params = array(
-														 'directory'    => $this->_request->directory(),
-														 'controller'   => $this->_request->controller(),
-														 'action'       => $this->_request->action(),
-													 ) + $this->_request->param();
+        if ($current === null) {
+            $this->_current = (int) $this->_detect_current_page();
+        } else {
+            $this->_current = (int) $current;
+        }
 
-		$this->_config = Kohana::$config->load('pagination');
+        $this->_total = (int) $total;
+        $this->_limit = (int) $limit ? $limit : $this->_config->limit;
+    }
 
-		if ($current === null)
-		{
-			$this->_current = (int) $this->_detect_current_page();
-		}
-		else
-		{
-			$this->_current = (int) $current;
-		}
+    /**
+     * @param int $total
+     * @param int $limit
+     * @param int $current
+     *
+     * @return Pagination
+     */
+    public static function factory($total, $limit = null, $current = null)
+    {
+        return new Pagination($total, $limit, $current);
+    }
 
-		$this->_total = (int) $total;
-		$this->_limit = (int) $limit ? $limit : $this->_config->limit;
-	}
+    /**
+     * Request setter / getter
+     *
+     * @param  Request
+     *
+     * @return  Request  If used as getter
+     * @return  Pagination  Chainable as setter
+     */
+    public function request(Request $request = null)
+    {
+        if ($request === null) {
+            return $this->_request;
+        }
 
-	/**
-	 * @param int $total
-	 * @param int $limit
-	 * @param int $current
-	 *
-	 * @return Pagination
-	 */
-	public static function factory($total, $limit = null, $current = null)
-	{
-		return new Pagination($total, $limit, $current);
-	}
+        $this->_request = $request;
 
-	/**
-	 * Request setter / getter
-	 *
-	 * @param  Request
-	 *
-	 * @return  Request  If used as getter
-	 * @return  Pagination  Chainable as setter
-	 */
-	public function request(Request $request = null)
-	{
-		if ($request === null)
-		{
-			return $this->_request;
-		}
+        return $this;
+    }
 
-		$this->_request = $request;
+    /**
+     * Route setter / getter
+     *
+     * @param  Route
+     *
+     * @return  Route  Route if used as getter
+     * @return  Pagination  Chainable as setter
+     */
+    public function route(Route $route = null)
+    {
+        if ($route === null) {
+            return $this->_route;
+        }
 
-		return $this;
-	}
+        $this->_route = $route;
 
-	/**
-	 * Route setter / getter
-	 *
-	 * @param  Route
-	 *
-	 * @return  Route  Route if used as getter
-	 * @return  Pagination  Chainable as setter
-	 */
-	public function route(Route $route = null)
-	{
-		if ($route === null)
-		{
-			return $this->_route;
-		}
+        return $this;
+    }
 
-		$this->_route = $route;
+    /**
+     * Route parameters setter / getter
+     *
+     * @param  array  Route parameters to set
+     *
+     * @return  array  Route parameters if used as getter
+     * @return  $this  Chainable as setter
+     */
+    public function route_params(array $route_params = null)
+    {
+        if ($route_params === null) {
+            return $this->_route_params;
+        }
 
-		return $this;
-	}
+        $this->_route_params = $route_params;
 
-	/**
-	 * Route parameters setter / getter
-	 *
-	 * @param  array  Route parameters to set
-	 *
-	 * @return  array  Route parameters if used as getter
-	 * @return  $this  Chainable as setter
-	 */
-	public function route_params(array $route_params = null)
-	{
-		if ($route_params === null)
-		{
-			return $this->_route_params;
-		}
+        return $this;
+    }
 
-		$this->_route_params = $route_params;
+    /**
+     * @return int Offset used in result set
+     */
+    public function get_offset()
+    {
+        return (($this->get_current_page() - 1) * $this->_limit);
+    }
 
-		return $this;
-	}
+    /**
+     * @return int Per page limit
+     */
+    public function get_limit()
+    {
+        return $this->_limit;
+    }
 
-	/**
-	 * @return int Offset used in result set
-	 */
-	public function get_offset()
-	{
-		return (($this->get_current_page() - 1) * $this->_limit);
-	}
+    /**
+     * @return int The total number of rows
+     */
+    public function get_total()
+    {
+        return $this->_total;
+    }
 
-	/**
-	 * @return int Per page limit
-	 */
-	public function get_limit()
-	{
-		return $this->_limit;
-	}
+    /**
+     * @return int Current page number
+     */
+    public function get_current_page()
+    {
+        return (int) min(max(1, $this->_current), max(1, $this->get_total_pages()));
+    }
 
-	/**
-	 * @return int The total number of rows
-	 */
-	public function get_total()
-	{
-		return $this->_total;
-	}
+    /**
+     * @return int Total number of pages
+     */
+    public function get_total_pages()
+    {
+        return (int) ceil($this->_total / $this->_limit);
+    }
 
-	/**
-	 * @return int Current page number
-	 */
-	public function get_current_page()
-	{
-		return (int) min(max(1, $this->_current), max(1, $this->get_total_pages()));
-	}
+    /**
+     * @return int Current page
+     */
+    protected function _detect_current_page()
+    {
+        switch ($this->_config->source) {
+            case 'route':
+                $page = $this->_request->param($this->_config->key);
+                break;
+            default:
+                $page = $this->_request->query($this->_config->key);
+                break;
+        }
 
-	/**
-	 * @return int Total number of pages
-	 */
-	public function get_total_pages()
-	{
-		return (int) ceil($this->_total / $this->_limit);
-	}
+        return (int) $page ? : 1;
+    }
 
-	/**
-	 * @return int Current page
-	 */
-	protected function _detect_current_page()
-	{
-		switch ($this->_config->source)
-		{
-			case 'route':
-				$page = $this->_request->param($this->_config->key);
-				break;
-			default:
-				$page = $this->_request->query($this->_config->key);
-				break;
-		}
+    /**
+     * Generates the full URL for a certain page.
+     *
+     * @param   integer int page number
+     *
+     * @uses Url::site
+     * @return  string   page URL
+     */
+    public function url($page = 1)
+    {
+        // Clean the page number
+        $page = max(1, (int) $page);
 
-		return (int) $page ? : 1;
-	}
+        // No page number in URLs to first page
+        if (($page === 1) and !$this->_config->first_page_in_url) {
+            $page = null;
+        }
 
-	/**
-	 * Generates the full URL for a certain page.
-	 *
-	 * @param   integer int page number
-	 *
-	 * @uses Url::site
-	 * @return  string   page URL
-	 */
-	public function url($page = 1)
-	{
-		// Clean the page number
-		$page = max(1, (int) $page);
+        switch ($this->_config->source) {
+            case 'query':
+                return URL::site($this->_route->uri($this->_route_params) . $this->query(array($this->_config->key => $page)));
+            case 'route':
+                return URL::site($this->_route->uri(array_merge($this->_route_params, array($this->_config->key => $page))) . $this->query());
+        }
 
-		// No page number in URLs to first page
-		if (($page === 1) and ! $this->_config->first_page_in_url)
-		{
-			$page = null;
-		}
+        return '#';
+    }
 
-		switch ($this->_config->source)
-		{
-			case 'query':
-				return URL::site($this->_route->uri($this->_route_params) . $this->query(array($this->_config->key => $page)));
-			case 'route':
-				return URL::site($this->_route->uri(array_merge($this->_route_params, array($this->_config->key => $page))) . $this->query());
-		}
+    /**
+     * URL::query() replacement for Pagination use only
+     *
+     * @param  array $params  Parameters to override
+     *
+     * @return  string
+     */
+    public function query(array $params = null)
+    {
+        if ($params === null) {
+            // Use only the current parameters
+            $params = $this->_request->query();
+        } else {
+            // Merge the current and new parameters
+            $params = array_merge($this->_request->query(), $params);
+        }
 
-		return '#';
-	}
+        if (empty($params)) {
+            // No query parameters
+            return '';
+        }
 
-	/**
-	 * URL::query() replacement for Pagination use only
-	 *
-	 * @param  array $params  Parameters to override
-	 *
-	 * @return  string
-	 */
-	public function query(array $params = null)
-	{
-		if ($params === null)
-		{
-			// Use only the current parameters
-			$params = $this->_request->query();
-		}
-		else
-		{
-			// Merge the current and new parameters
-			$params = array_merge($this->_request->query(), $params);
-		}
+        // Note: http_build_query returns an empty string for a params array with only null values
+        $query = http_build_query($params, '', '&');
 
-		if (empty($params))
-		{
-			// No query parameters
-			return '';
-		}
+        // Don't prepend '?' to an empty string
+        return ($query === '') ? '' : ('?' . $query);
+    }
+}
 
-		// Note: http_build_query returns an empty string for a params array with only null values
-		$query = http_build_query($params, '', '&');
-
-		// Don't prepend '?' to an empty string
-		return ($query === '') ? '' : ('?' . $query);
-	}
-} // End Pagination
--- config/pagination.php
+++ config/pagination.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'source'            => 'route', // source: query or route
-	'key'               => 'page',
-	'first_page_in_url' => false,
+    'source'            => 'route', // source: query or route
+    'key'               => 'page',
+    'first_page_in_url' => false,
 );
